4b88c1d86ff169683682a1cee55b90bcbf56c5a2
efogerson1 <efogerson2@gmail.com>
2023-08-28 18:01:46 -0700

    added new var

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 
-// } else {
+} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -19,7 +19,7 @@ let sequelize;
       port: 3306
     }
   );
-// }
+}
 
 
 module.exports = sequelize;
